cd57c0192b358694c59289b376bf9c6e0262af04
Giannina <giannisarappa@gmail.com>
2023-06-24 12:18:23 +0200

    Video 192. Database setup & saving data to Firestore.

--- Flash Chat iOS13/Controllers/ChatViewController.swift
+++ Flash Chat iOS13/Controllers/ChatViewController.swift
@@ -16,6 +16,8 @@ class ChatViewController: UIViewController {
     @IBOutlet weak var tableView: UITableView!
     @IBOutlet weak var messageTextfield: UITextField!
     
+    let databaseReference = Firestore.firestore()
+    
     var messages: [Message] = [
         Message(sender: "1@2.com", body: "Hola :)"),
         Message(sender: "a@b.com", body: "Qué tal? Cómo te va?"),
@@ -33,6 +35,19 @@ class ChatViewController: UIViewController {
     }
     
     @IBAction func sendPressed(_ sender: UIButton) {
+        if let messageBody = messageTextfield.text, let messageSender = Auth.auth().currentUser?.email {
+            
+            databaseReference.collection(K.FStore.collectionName).addDocument(data: [
+                K.FStore.senderField: messageSender,
+                K.FStore.bodyField: messageBody
+            ]) { (error) in
+                if let e = error {
+                    print("There was an issue saving data to firestore, \(e)")
+                } else {
+                    print("Successfully saved date :)")
+                }
+            }
+        }
     }
     
     @IBAction func logOutPressed(_ sender: UIBarButtonItem) {
@@ -43,9 +58,7 @@ class ChatViewController: UIViewController {
         } catch let signOutError as NSError {
           print("Error signing out: %@", signOutError)
         }
-        
     }
-    
 }
 
 extension ChatViewController: UITableViewDataSource { //protocolo para rellenar la TableView
